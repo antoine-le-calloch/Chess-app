8f3c841bcbeed8cbf66fb06ca7be767177af7dc3
Antoine Le Calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-05-15 01:55:22 -0700

    Ignore folder commit.

--- cmake-build-debug/CMakeFiles/IA_chess.dir/src/gameProcess.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/IA_chess.dir/src/gameProcess.c.obj: \
- C:\Users\user\Documents\IT_Project\IA_chess\src\gameProcess.c
--- cmake-build-debug/CMakeFiles/IA_chess.dir/src/pieceMovement.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/IA_chess.dir/src/pieceMovement.c.obj: \
- C:\Users\user\Documents\IT_Project\IA_chess\src\pieceMovement.c
